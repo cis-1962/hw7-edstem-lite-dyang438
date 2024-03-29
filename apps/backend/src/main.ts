d18b0d6ffbfda47ab5f4d58d4a83e591cb98d7f8
dyang438 <152822883+dyang438@users.noreply.github.com>
2024-03-28 22:18:56 -0400

    Finished it baby!!!! This has been a slog, I also had to figure the same things but with a slightly different architecture for my SWE class. GG.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,5 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+import bodyParser from 'body-parser';
+
+import accountRouter from './routes/account';
+import questionsRouter from './routes/questions';
 
 // read environment variables from .env file
 dotenv.config();
@@ -7,13 +13,35 @@ const PORT = process.env.PORT ?? 8000;
 
 const app = express();
 
+app.use(cookieSession({
+  name: 'session',
+  keys: ['k1', 'k2'],
+  maxAge: 24 * 60 * 60 * 1000
+}));
+
+app.use(bodyParser.json());
+
+const MONGO_URI = 'mongodb+srv://dyang438:fortnitebattleroyale@hw7edstemlitecis1962db.zluhcf5.mongodb.net/?retryWrites=true&w=majority&appName=HW7EdStemLiteCIS1962DB'
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connection success.');
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error.message);
+  });
+
 // define root route
-app.get('/api/hello', (_, res) => {
+app.get('/', (_, res) => {
   res.json({ message: 'Hello, frontend!' });
 });
 
+// account routes
+app.use('/api/account', accountRouter);
+
+// question routes
+app.use('/api/questions', questionsRouter);
+
 // listen
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`Now listening on port ${PORT}.`);
 });
---
+++ apps/backend/src/routes/account.ts
@@ -0,0 +1,56 @@
+import express from 'express';
+import User from '../models/user';
+import jwt from 'jsonwebtoken'
+
+const router = express.Router();
+
+// Signup
+router.post('/signup', async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Cannot post empty password or username"})
+  }
+
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return res.status(400).json({ message: "Username is already in use." });
+  }
+
+  const newUser = new User({ username, password });
+  await newUser.save();
+
+  // Set user info in session
+  if (req.session) {
+    req.session.user = { userId: newUser._id };
+  }
+
+  res.status(200).json({ message: "Signup successful." });
+
+});
+
+// Login
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user) {
+        return res.status(404).json({ message: "User not found." });
+    }
+    if (password !== user.password) {
+        return res.status(401).json({ message: "Invalid password." });
+    }
+    const token = jwt.sign({ userId: user._id }, 'fortnitebattleroyale', { expiresIn: '24h' });
+
+    res.status(200).json({ token });
+
+});
+
+// Logout
+router.post('/logout', (req, res) => {
+
+  req.session = null;
+  res.status(200).json({ message: "logout successful." });
+
+});
+
+export default router;
